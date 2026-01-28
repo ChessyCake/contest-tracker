5edc8657f648dc12d17f1c58c470ecc40edd0baf
Harsh Raj <harshhraj2468@gmail.com>
2026-01-28 23:30:12 +0530

    redesigned frotend portion

--- src/Card.jsx
+++ src/Card.jsx
@@ -43,18 +43,18 @@ function Card( {contest}){
       <div className="platform-name text-xl z-30 translate-y-10 inline-block bg-black text-white rounded-xl p-2 shadow-lg shadow-blue-500/20">
         <h1>codeforces</h1>
       </div>
-    <div className="card flex justify-between items-center flex-col shadow-xl  shadow-blue-500/30 rounded-2xl">
+    <div className="card flex justify-between items-center flex-col border-black border-2 rounded-2xl shadow-lg shadow-blue-500/40">
     <div className="platform flex flex-col justify-center items-center mt-20">
 
-         <div className="contest-difficulty text-3xl p-2">
+         <div className="contest-difficulty text-3xl p-1">
             <h1>{contest.name}</h1>
          </div>
     </div>
     <div className="info flex justify-center items-center flex-col w-full m-10">
-    <div className="happening-date bg-green-300 text-center w-full h-12 flex items-center justify-center">
-        <h1 className="text-3xl">{ getDate() }</h1>
+    <div className="happening-date text-lime-500 text-center w-full h-12 flex items-center justify-center">
+        <h1 className="text-2xl">{ getDate() }</h1>
     </div>
-    <div className="time-left bg-amber-300 text-center w-full h-12 flex items-center justify-center ">
+    <div className="time-left text-red-500 text-center w-full h-12 flex items-center justify-center ">
         <h1 className="text-2xl">{timer}</h1>
     </div>
     <button className="btn bg-black text-white p-1 rounded-xl text-2xl m-2 relative z-20 translate-x-30 cursor-pointer" onClick={ contestLink}>  GO </button>
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,4 +1,4 @@
-/*eslint-disable no-unused-vars*/
+
 import { useEffect, useState } from "react";
 import getContestDetails from "./api/codeforces";
 import Card from "./Card";
@@ -16,14 +16,20 @@ function HomePage(){
     }, [])
     
    return( 
-   <div className="flex flex-wrap justify-center items-center gap-4">
+   <div>
+    <div className="header w-full h-20 bg-black text-white flex justify-center items-center text-3xl">
+         <h1 className="text-red-400">REMEMBER:</h1>  <h1>Its always YOU' v/s YOU'</h1>
+    </div>
+    <div className="flex flex-wrap justify-center items-center gap-4">
         {data.map( d => (
             <Card key={d.id} contest = {d} />
         ))}
 
-        <div className="footer bottom-0 bg-black w-screen text-white flex justify-center items-end text-7xl">
-              <h3 className="mb-5"> CONTEST TRACKER</h3>
+        <div className="footer bottom-0 bg-black w-screen text-white flex justify-center items-end flex-row">
+              <h3 className="mb-3 text-7xl"> CONTEST TRACKER</h3>
+              <p className="text-right">&copy;{new Date().getFullYear()} Harsh Raj Minj</p>
         </div>
+     </div>
     </div>
 
     )
